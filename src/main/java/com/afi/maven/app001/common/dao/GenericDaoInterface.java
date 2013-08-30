d0e04707e7eab315295032869f1c9be98547f9b2
AFI <AFI@AFI-PC>
2013-08-30 02:26:47 -0700

    perubahan/remove Generic dao ke package common.dao

--- src/main/java/com/afi/maven/app001/common/dao/GenericDaoInterface.java
+++ src/main/java/com/afi/maven/app001/common/dao/GenericDaoInterface.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.afi.maven.app001.common.model;
+package com.afi.maven.app001.common.dao;
 
 import java.io.Serializable;
 
